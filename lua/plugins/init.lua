8fd0cdf03a6e183f9d6fdbecb9c28ed4b76094c0
snappymob-woon <146707643+snappymob-woon@users.noreply.github.com>
2024-08-30 12:57:12 +0800

    fix(cmp): Make <C-p> and <C-n> use luasnip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,26 @@ return {
       local conf = require "nvchad.configs.cmp"
       local cmp = require "cmp"
 
+      conf.mapping["<C-n>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif require("luasnip").expand_or_jumpable() then
+          require("luasnip").expand_or_jump()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
+      conf.mapping["<C-p>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif require("luasnip").jumpable(-1) then
+          require("luasnip").jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+
       conf.mapping["<CR>"] = nil
       conf.mapping["<S-Tab>"] = nil
       conf.mapping["<Tab>"] = cmp.mapping.confirm {
